ed7f67f81f10377b46ec72207580c107a214cb6e
Benedikt Otto <b.otto.code@protonmail.com>
2026-01-26 13:57:00 +0100

    fix: remove explicit ublue akmods copr cleanup

--- build_files/build.sh
+++ build_files/build.sh
@@ -26,9 +26,7 @@ systemctl enable fubs-os-groups
 # NOTE: With isolated COPR installation, most repos are never enabled globally.
 # We only need to clean up repos that were enabled during the build process.
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-
-# NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ublue-os:akmods.repo
 
 # Disable RPM Fusion repos.
 for i in /etc/yum.repos.d/rpmfusion-*.repo; do
